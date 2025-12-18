2f7e841284a4dfdf4cc11b0ba3d8e155683f047e
anonim-cmyk <ilhamsyahdan123@gmail.com>
2025-12-18 23:46:35 +0700

    fix

--- app/bayar/page.tsx
+++ app/bayar/page.tsx
@@ -1,12 +1,7 @@
 export const dynamic = "force-dynamic";
 
-import { Suspense } from "react";
 import BayarClient from "./BayarClient";
 
-export default function Page() {
-  return (
-    <Suspense fallback={<div className="text-white">Loading...</div>}>
-      <BayarClient />
-    </Suspense>
-  );
+export default function Page({ params }: { params: { orderId: string } }) {
+  return <BayarClient orderId={params.orderId} />;
 }
